6e15ab6598bd231a4bcaa46bbee2ba24630b0406
kskisb <isbkosuke@gmail.com>
2024-05-21 16:40:54 +0900

    front: ユーザー一覧画面を作成

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-do
 import HomePage from './home/HomePage';
 import TodosPage from './todos/TodosPage';
 import TodoPage from './todos/TodoPage';
+import UsersPage from './users/UsersPage';
 import './App.css';
 
 function App() {
@@ -19,12 +20,16 @@ function App() {
         <NavLink to="/todos" className="button rounded">
           Todos
         </NavLink>
+        <NavLink to="/users" className="button rounded">
+          Users
+        </NavLink>
       </header>
       <div className="container">
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/todos' element={<TodosPage />} />
           <Route path='/todos/:id' element={<TodoPage />} />
+          <Route path='/users' element={<UsersPage />} />
         </Routes>
       </div>
     </Router>
---
+++ frontend/app/src/users/UsersPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { User } from './User';
+
+function UsersPage() {
+  return (
+    <div>UsersPage</div>
+  )
+}
+
+export default UsersPage
