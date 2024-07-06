d3599c457d0a30f945cd21c7ad80cbd17afa6332
kskisb <isbkosuke@gmail.com>
2024-07-06 13:01:32 +0900

    feat: todoとuserの関連づけ

--- backend/app/models/todo.rb
+++ backend/app/models/todo.rb
@@ -1,4 +1,6 @@
 class Todo < ApplicationRecord
+  belongs_to :user
+
   validates :title, presence: true
   validates :priority, inclusion: { in: %w(Low Medium High), message: "%{value} is not a valid priority" }
 end
