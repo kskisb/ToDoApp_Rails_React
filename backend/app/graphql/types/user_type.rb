3af7cc44330b8b70d1c8883abf0747c1ff48b493
kskisb <isbkosuke@gmail.com>
2024-07-08 20:11:03 +0900

    feat: userからtodoを取得する際のN+1を解消

--- backend/app/graphql/types/user_type.rb
+++ backend/app/graphql/types/user_type.rb
@@ -8,5 +8,9 @@ module Types
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
     field :todos, [TodoType], null: true
+
+    def todos
+      Loaders::AssociationLoader.for(User, :todos).load(object)
+    end
   end
 end
