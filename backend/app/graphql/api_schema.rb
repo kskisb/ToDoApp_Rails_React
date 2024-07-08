16f96c9f44a3b88251467d35fcee36c6d414a250
kskisb <isbkosuke@gmail.com>
2024-07-08 21:23:20 +0900

    feat: タイムアウトさせる

--- backend/app/graphql/api_schema.rb
+++ backend/app/graphql/api_schema.rb
@@ -8,6 +8,8 @@ class ApiSchema < GraphQL::Schema
   # use GraphQL::Dataloader
   use GraphQL::Batch
 
+  use GraphQL::Schema::Timeout, max_seconds: 3
+
   # GraphQL-Ruby calls this when something goes wrong while running a query:
   def self.type_error(err, context)
     # if err.is_a?(GraphQL::InvalidNullError)
