f35e673407222d69e77e3fef4f937ac50191576f
kskisb <isbkosuke@gmail.com>
2024-07-09 17:37:14 +0900

    chore: nodeとnodesをresolverへ移動

--- backend/app/graphql/types/query_type.rb
+++ backend/app/graphql/types/query_type.rb
@@ -2,26 +2,11 @@
 
 module Types
   class QueryType < Types::BaseObject
-    field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
-      argument :id, ID, required: true, description: "ID of the object."
-    end
-
-    def node(id:)
-      context.schema.object_from_id(id, context)
-    end
-
-    field :nodes, [Types::NodeType, null: true], null: true, description: "Fetches a list of objects given a list of IDs." do
-      argument :ids, [ID], required: true, description: "IDs of the objects."
-    end
-
-    def nodes(ids:)
-      ids.map { |id| context.schema.object_from_id(id, context) }
-    end
-
-    field :user, resolver: Resolvers::UserResolver
-    field :users, resolver: Resolvers::UsersResolver
-
+    field :node, resolver: Resolvers::NodeResolver
+    field :nodes, resolver: Resolvers::NodesResolver
     field :todo, resolver: Resolvers::TodoResolver
     field :todos, resolver: Resolvers::TodosResolver
+    field :user, resolver: Resolvers::UserResolver
+    field :users, resolver: Resolvers::UsersResolver
   end
 end
