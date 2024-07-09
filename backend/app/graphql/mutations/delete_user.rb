f3f6d611519e25b07eb8549643c96b4a70e3a506
kskisb <isbkosuke@gmail.com>
2024-07-09 14:05:19 +0900

    feat: userの削除機能

---
+++ backend/app/graphql/mutations/delete_user.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+module Mutations
+  class DeleteUser < BaseMutation
+    field :id, ID, null: false
+
+    argument :id, ID, required: true
+
+    def resolve(id:)
+      user = User.find(id)
+      user.destroy
+      {
+        id: id
+      }
+  end
+end
--- backend/app/graphql/types/mutation_type.rb
+++ backend/app/graphql/types/mutation_type.rb
@@ -2,6 +2,7 @@
 
 module Types
   class MutationType < Types::BaseObject
+    field :delete_user, mutation: Mutations::DeleteUser
     field :create_user, mutation: Mutations::CreateUser
   end
 end
