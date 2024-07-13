9925ca8feee48798f0af911488a53269b03d9d91
kskisb <isbkosuke@gmail.com>
2024-07-13 14:06:34 +0900

    feat: 認証

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,37 +1,118 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import React, { useState, useEffect, createContext } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate, NavLink } from 'react-router-dom';
+import CommonLayout from 'components/layouts/CommonLayout';
 import HomePage from './components/home/HomePage';
+import SignUpPage from 'components/auth/SignUpPage';
+import SignInPage from 'components/auth/SignInPage';
 import TodosPage from './components/todos/TodosPage';
 import TodoPage from './components/todos/TodoPage';
-import './App.css';
 import UsersPage from './components/users/UsersPage';
+import './App.css';
+
+import { getCurrentUser } from 'api/auth';
+import { User } from 'interfaces/user/index'
+
+export const AuthContext = createContext({} as {
+  loading: boolean
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>
+  isSignedIn: boolean
+  setIsSignedIn: React.Dispatch<React.SetStateAction<boolean>>
+  currentUser: User | undefined
+  setCurrentUser: React.Dispatch<React.SetStateAction<User | undefined>>
+})
 
 function App() {
+  const [loading, setLoading] = useState<boolean>(true)
+  const [isSignedIn, setIsSignedIn] = useState<boolean>(false)
+  const [currentUser, setCurrentUser] = useState<User | undefined>()
+
+  // Check for an authenticated user
+  // If confirmed, retrieve the user's info
+  const handleGetCurrentUser = async () => {
+    try {
+      const res = await getCurrentUser()
+
+      if(res?.data.isLogin === true) {
+        setIsSignedIn(true)
+        setCurrentUser(res?.data.data)
+
+        console.log(res?.data.data)
+      } else {
+        console.log('No authenticated user')
+      }
+    } catch(err) {
+      console.log(err)
+    }
+
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    handleGetCurrentUser()
+  }, [setCurrentUser])
+
+  // Determine Routing whether the user is authenticated or not
+  // Prompt the user to the '/signin' if unauthenticated
+  const PrivateRoute = ({ children }: { children: React.ReactElement }) => {
+    if(!loading) {
+      return isSignedIn ? children : <Navigate to='/signin' replace />
+    }
+    return null
+  }
+
   return (
     <Router>
-      <header className="sticky">
-        <span className="">
-          {/* <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" /> */}
-        </span>
-        <NavLink to="/" className="button rounded">
-          <span className="icon-home"></span>
-          Home
-        </NavLink>
-        <NavLink to="/todos" className="button rounded">
-          Todos
-        </NavLink>
-        <NavLink to="/users" className="button rounded">
-          Users
-        </NavLink>
-      </header>
-      <div className="todos-container">
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/todos' element={<TodosPage />} />
-          <Route path='/todos/:id' element={<TodoPage />} />
-          <Route path='/users' element={<UsersPage />} />
-        </Routes>
-      </div>
+      <AuthContext.Provider value={{ loading, setLoading, isSignedIn, setIsSignedIn, currentUser, setCurrentUser }}>
+        <header className="sticky">
+          <NavLink to="/" className="button rounded">
+            <span className="icon-home"></span>
+            Home
+          </NavLink>
+          <NavLink to="/todos" className="button rounded">
+            Todos
+          </NavLink>
+          <NavLink to="/users" className="button rounded">
+            Users
+          </NavLink>
+          {!isSignedIn && (
+            <>
+              <NavLink to='/signup' className="button rounded">
+                Sign Up
+              </NavLink>
+              <NavLink to='/signin' className="button rounded">
+                Sign In
+              </NavLink>
+            </>
+          )}
+          {isSignedIn && (
+            <button onClick={() => setIsSignedIn(false)} className="button rounded">
+              Sign Out
+            </button>
+          )}
+        </header>
+        <div className="todos-container">
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/signup' element={<SignUpPage />} />
+            <Route path='/signin' element={<SignInPage />} />
+            <Route path='/todos' element={
+              <PrivateRoute>
+                <TodosPage />
+              </PrivateRoute>
+            } />
+            <Route path='/todos/:id' element={
+              <PrivateRoute>
+                <TodoPage />
+              </PrivateRoute>
+            } />
+            <Route path='/users' element={
+              <PrivateRoute>
+                <UsersPage />
+              </PrivateRoute>
+            } />
+          </Routes>
+        </div>
+      </AuthContext.Provider>
     </Router>
   )
 }
