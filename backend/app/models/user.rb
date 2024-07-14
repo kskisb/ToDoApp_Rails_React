abc1e0934f2f1e6c10f581ce885a5098d11f2007
kskisb <isbkosuke@gmail.com>
2024-07-15 00:55:54 +0900

    chore: user.rb

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  has_many :todos, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, {presence: true, uniqueness: true}
 end
