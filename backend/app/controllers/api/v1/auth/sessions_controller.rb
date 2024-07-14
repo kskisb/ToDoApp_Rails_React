2bc51fbb4ea299cd423bcfac05da4b21df9c163e
kskisb <isbkosuke@gmail.com>
2024-07-14 23:55:34 +0900

    chore: ログイン状態を保持する

--- backend/app/controllers/api/v1/auth/sessions_controller.rb
+++ backend/app/controllers/api/v1/auth/sessions_controller.rb
@@ -1,11 +1,3 @@
-# module Api
-#   module V1
-#     module Auth
-#       class SessionsController < ApplicationController
-#       end
-#     end
-#   end
-# end
 class Api::V1::Auth::SessionsController < ApplicationController
   def index
     if current_api_v1_user
